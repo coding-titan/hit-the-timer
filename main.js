cb6b51ee0b97dbc08ab8f577adf69379cfc3e547
Seb <sebastian.manieri@gmail.com>
2024-09-17 12:53:02 +0200

    working goal number generator and checker

--- main.js
+++ main.js
@@ -1,11 +1,33 @@
 let button = document.getElementById("timer-button");
 let timebox =  document.getElementById("time")
 let timetext = document.getElementById("timeText")
+let targetText = document.getElementById("target-text")
+let resultText = document.getElementById("result-text")
 
-function showTime(element) {
-    timebox.style.display = "block";
-    timetext.innerHTML = (element.timeStamp / 1000).toFixed(2);
+function getResultComment(currentTarget, currentTime) {
+    if (currentTarget > currentTime) {
+        return "Too soon."
+    } else if (currentTarget < currentTime) {
+        return "Too late."
+    } else {
+        return "YOU DID IT!!!";
+    }
 }
 
-button.addEventListener("click", showTime)
+function hitButton(element) {
+    timebox.style.display = "block";
+    let currentTime = (element.timeStamp / 1000).toFixed(0);
+    timetext.innerHTML = currentTime;
+    let currentTarget = target;
+    resultText.innerHTML = getResultComment(currentTarget, currentTime);
+}
 
+function setRandomTarget() {
+    let newTarget = (Math.random() * 8 +2).toFixed(0);
+    return newTarget;
+}
+
+button.addEventListener("click", hitButton)
+
+let target = setRandomTarget();
+targetText.innerHTML = target;
